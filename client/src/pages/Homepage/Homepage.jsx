04c176f66aa351dd5268ace5962f34e821cca581
ErnAbr <ern_abr@yahoo.com>
2025-03-02 21:18:19 +0200

    testing

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -9,7 +9,7 @@ import { io } from "socket.io-client";
 import { toast } from "react-toastify";
 import { playHitSound, playMissSound } from "../../utils/sounds.js";
 import { LoadingComponent } from "../../components/LoadingComponent/LoadingComponent.jsx";
-import { PillComponent } from "@/components/Pill/PillComponent.jsx";
+// import { PillComponent } from "@/components/Pill/PillComponent.jsx";
 
 export const Homepage = () => {
   const initialAvailableShips = useMemo(
@@ -157,9 +157,9 @@ export const Homepage = () => {
               arrow
               placement="right-end"
             >
-              <div>
+              {/* <div>
                 <PillComponent text="i" size="small" />
-              </div>
+              </div> */}
             </Tooltip>
           </div>
           {availableShips.map((ship) => (

58122d9a8f9990caaa60833c607f739612c3969c
ErnAbr <ern_abr@yahoo.com>
2025-03-02 21:09:14 +0200

    ...

--- client/src/components/AppBar/TopAppBar.jsx
+++ client/src/components/AppBar/TopAppBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import styles from "./topAppBar.module.scss";
 import { useAppStore } from "../../context/store.js";
-import { PillComponent } from "../Pill/PillComponent.jsx";
+import { PillComponent } from "@components/Pill/PillComponent.jsx";
 
 export const TopAppBar = () => {
   const user = useAppStore((state) => state.user);

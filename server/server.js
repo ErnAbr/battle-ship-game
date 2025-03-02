c61d6ce4023d2325c0b81f3a520b2c3a56c73fb5
ErnAbr <ern_abr@yahoo.com>
2025-03-02 21:56:00 +0200

    serving dist files from the server

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ const express = require("express");
 const { createServer } = require("http");
 const setupSocket = require("./socket");
 const connectDB = require("./libs/database");
+const path = require("path");
 
 const app = express();
 const httpServer = createServer(app);
 
 configMiddlewares(app);
 connectDB();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 const UserController = require("./controllers/UserController/UserController");
 const GameStatsController = require("./controllers/GameController/GameController");
@@ -17,6 +21,10 @@ app.use("/api/users", UserController);
 app.use("/api/games", GameStatsController);
 
 setupSocket(httpServer);
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3005;
 
